55d6336289cd21e09b1bc66261fcfc77bbb98dfc
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-02-28 10:55:07 +0100

    Actualización practicas
    Actualización de los paquetes e información de los mismos, creación de clases y comentarios

---
+++ POO-Java-Ejercicios/Fase6_Manejo_de_Excepciones/CuentaBancaria.java
@@ -0,0 +1,4 @@
+public class CuentaBancaria {
+
+    //Ejercicio 10: Clase CuentaBancaria con excepción personalizada SaldoInsuficienteException.
+}
---
+++ POO-Java-Ejercicios/Fase8_Enums/Pedido.java
@@ -0,0 +1,4 @@
+public class Pedido {
+
+    //Ejercicio 12: Clase Pedido con enum EstadoPedido (PENDIENTE, ENVIADO, ENTREGADO).
+}
