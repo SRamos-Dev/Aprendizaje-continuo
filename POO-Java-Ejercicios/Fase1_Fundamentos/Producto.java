57da877899837bad9552dced86656c33a6ce4b67
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-03-01 00:01:49 +0100

    Actualizacion de practica
    Creación de Producto y sobrecarga de constructores

--- POO-Java-Ejercicios/Fase1_Fundamentos/Producto.java
+++ POO-Java-Ejercicios/Fase1_Fundamentos/Producto.java
@@ -2,6 +2,59 @@
 public class Producto {
 
     //Ejercicio 2: Clase Producto con sobrecarga de constructores (vacío, con nombre y precio, con todos los atributos).
+
+    // 1. Atributos privados
+    private String nombre;
+    private double precio;
+    private int cantidad;
     
+    // 2. Constructor para inicializar el producto vacio
+    public Producto(){
+        this.nombre = "";
+        this.precio = 0.0;
+        this.cantidad = 0;
+    }
+
+    // 3. Constructor para inicializar el producto con nombre y precio
+    public Producto(String nombre, double precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    // 4. Constructor para inicializar el producto con todos los atributos
+    public Producto(String nombre, double precio, int cantidad){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    // 5. Getters
+    public String getNombre(){
+        return nombre;
+    }
+    public double getPrecio(){
+        return precio;
+    }
+    public int getCantidad(){
+        return cantidad;
+    }
+
+    // 6. Setters
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
+    public void setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
+
+    // 6. Metodo para mostrar la informacion del producto
+    public void mostrarInfo(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Precio: "+precio);
+        System.out.println("Cantidad: "+cantidad);
+    }
 }
         
