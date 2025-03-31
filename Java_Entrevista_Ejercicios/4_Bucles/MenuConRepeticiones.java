eee042622a880cc7350070429600596356c6eac3
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-03-31 12:59:42 +0200

    Update MenuConRepeticiones.java

--- Java_Entrevista_Ejercicios/4_Bucles/MenuConRepeticiones.java
+++ Java_Entrevista_Ejercicios/4_Bucles/MenuConRepeticiones.java
@@ -12,38 +12,39 @@ public class MenuConRepeticiones {
         Scanner entrada = new Scanner(System.in);
         int opcion;
 
-        do {
-            System.out.println("Elige la opcion que deseas: ");
-            System.out.println("1. Sumar dos numeros.");
-            System.out.println("2. Restar dos numeros.");
-            System.out.println("3. Salir");
-            opcion = entrada.nextInt();
+            do {
+                System.out.println("Elige la opcion que deseas: ");
+                System.out.println("1. Sumar dos numeros.");
+                System.out.println("2. Restar dos numeros.");
+                System.out.println("3. Salir");
+                opcion = entrada.nextInt();
 
-            if(opcion == 1){
-                System.out.println("Indique dos numeros para ser sumados:");
-                System.out.print("Numero 1: ");
-                int num1 = entrada.nextInt();
-                System.out.print("Numero 2: ");
-                int num2 = entrada.nextInt();
-                System.out.println();
-                int suma = num1 + num2;
-                System.out.println("\nEl resultado de "+num1+" + "+num2+" es: "+suma+"\n");
-            }else if(opcion == 2){
-                System.out.println("Indique dos numeros para ser restados:");
-                System.out.print("Numero 1: ");
-                int num1 = entrada.nextInt();
-                System.out.print("Numero 2: ");
-                int num2 = entrada.nextInt();
-                System.out.println();
-                int resta = num1 - num2;
-                System.out.println("\nEl resultado de "+num1+" - "+num2+" es: "+resta+"\n");
-            }else if(opcion == 3){
-                System.out.println("\nSaliendo del programa...");
-            }else{
-                System.out.println("\nOpcion no valida. Elija una opcion correcta\n");
-            }
-        } while(opcion != 3);
-    }
+                if(opcion == 1){
+                    System.out.println("Indique dos numeros para ser sumados:");
+                    System.out.print("Numero 1: ");
+                    int num1 = entrada.nextInt();
+                    System.out.print("Numero 2: ");
+                    int num2 = entrada.nextInt();
+                    System.out.println();
+                    int suma = num1 + num2;
+                    System.out.println("\nEl resultado de "+num1+" + "+num2+" es: "+suma+"\n");
+                }else if(opcion == 2){
+                    System.out.println("Indique dos numeros para ser restados:");
+                    System.out.print("Numero 1: ");
+                    int num1 = entrada.nextInt();
+                    System.out.print("Numero 2: ");
+                    int num2 = entrada.nextInt();
+                    System.out.println();
+                    int resta = num1 - num2;
+                    System.out.println("\nEl resultado de "+num1+" - "+num2+" es: "+resta+"\n");
+                }else if(opcion == 3){
+                    System.out.println("\nSaliendo del programa...");
+                }else{
+                    System.out.println("\nOpcion no valida. Elija una opcion correcta\n");
+                }
+            } while(opcion != 3);
+            entrada.close();
+        }
     }
     
 
