a441fe39c3a44c01de7ba41ab7ee3ea3a7807b7d
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-04-01 12:55:08 +0200

    Practica 01/04/2025

--- Java_Entrevista_Ejercicios/5_Arrays/MayorYMenor.java
+++ Java_Entrevista_Ejercicios/5_Arrays/MayorYMenor.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
 public class MayorYMenor {
     public static void main(String[] args) {
         //Llena un array de 10 números aleatorios y muestra cuál es el mayor y cuál el menor.
+
+        int[] matriz = new int[10];
+        Random random = new Random();
+        int mayor = Integer.MIN_VALUE;
+        int menor = Integer.MAX_VALUE;
+        for (int i = 0; i < matriz.length; i++) {
+            matriz[i] = random.nextInt(100); // Números aleatorios entre 0 y 99
+            if (matriz[i] > mayor) {
+                mayor = matriz[i];
+            }
+            if (matriz[i] < menor) {
+                menor = matriz[i];
+            }
+        }
+        System.out.println("Los números generados son: ");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print(matriz[i] + " ");
+        }
+        System.out.println("\nEl número mayor es: " + mayor);
+        System.out.println("El número menor es: " + menor);
+        System.out.println("El número mayor es: " + mayor + " y el menor es: " + menor);
+
+
+
     }
 }
